059384a1155bbe925070cc8a74f0b703ce43ba8d
Bryan Silva <BASP@BASP>
2017-05-21 03:00:24 -0500

    Merge origin/master
    Conflicts:
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/java/com/basp/trabajo_al_minuto/web/model/ComponenteWeb.java

--- trabajo_al_minuto/trabajo_al_minuto-model/src/main/java/com/basp/trabajo_al_minuto/model/business/BusinessQuery.java
+++ trabajo_al_minuto/trabajo_al_minuto-model/src/main/java/com/basp/trabajo_al_minuto/model/business/BusinessQuery.java
@@ -20,15 +20,17 @@ public class BusinessQuery {
     public static final String GET_USUARIO_BY_EMPRESA = "SELECT u FROM Usuario u WHERE u.empresa.empresaId = :arg";
     public static final String GET_CITACIONES_BY_OFERTA = "SELECT u.citacionId FROM UsuarioHasOferta u WHERE u.usuarioHasOfertaId = :arg";
     public static final String GET_EVALUACION_BY_CITACION = "SELECT e FROM Evaluacion e WHERE e.citacion.citacionId = :arg";
-    public static final String GET_CITACIONES_ACTIVAS_BY_USUARIO = "SELECT u.citacionList FROM UsuarioHasOferta u WHERE u.usuarioUsuarioId.usuarioId = :arg AND u.estado = TRUE";
+    public static final String GET_CITACIONES_ACTIVAS_BY_USUARIO = "SELECT u.citacion FROM UsuarioHasOferta u WHERE u.usuarioUsuarioId.usuarioId = :arg AND u.citacion.estado = TRUE ORDER BY u.fechaPostulacion";
     public static final String GET_PERFILES_BY_EMPRESA = "SELECT o.perfil FROM Oferta o WHERE o.usuarioAutor.empresa.empresaId = :arg";
     public static final String GET_PRUEBAS_BY_PERFIL = "SELECT p.prueba FROM PerfilHasPrueba p WHERE p.perfil.perfilId = :arg";
     public static final String GET_USUARIOS_BY_OFERTA = "SELECT u FROM UsuarioHasOferta u WHERE u.ofertasOfertaId.ofertaId = :arg";
     public static final String GET_OFERTAS_MAS_APLICADAS_BY_EMPRESA = "SELECT u.ofertasOfertaId, COUNT(u.usuarioUsuarioId) AS c FROM UsuarioHasOferta u WHERE u.ofertasOfertaId.usuarioAutor.empresa.empresaId = :arg GROUP BY u.ofertasOfertaId ORDER BY c";
+    public static final String GET_OFERTAS_MAS_APLICADAS = "SELECT u.ofertasOfertaId, COUNT(u.usuarioUsuarioId) AS c FROM UsuarioHasOferta u GROUP BY u.ofertasOfertaId ORDER BY c";
     public static final String GET_USUARIOS_MEJORES_RESULTADOS = "SELECT e FROM Evaluacion e WHERE e.citacion.usuarioAutor.empresa.empresaId = :arg ORDER BY e.porcentaje DESC";
     public static final String GET_PRUEBAS_PLANTILLA_BY_EMPRESA = "SELECT p FROM PruebaPlantilla p WHERE p.empresaEmpresaId.empresaId = :arg";
     public static final String GET_CATALOGOS_BY_PADRE = "SELECT c FROM Catalogo c WHERE c.catalogoPadre.catalogoId = :arg AND c.estado = TRUE AND c.catalogoPadre.estado = TRUE";
-    public static final String GET_OFERTAS_ACTIVAS = "SELECT o FROM Oferta o WHERE o.estado = TRUE";
+    public static final String GET_OFERTAS_ACTIVAS = "SELECT o FROM Oferta o WHERE o.estado = TRUE ORDER BY o.fechaExpiracion";
+    public static final String GET_USUARIO_HAS_OFERTA = "SELECT u FROM UsuarioHasOferta u WHERE u.usuarioUsuarioId.usuarioId = :arg0 AND u.ofertasOfertaId.ofertaId = :arg1";
 
     public static final List<Object> GET_OFERTAS_EXTERNAL(Long area, String palabra) {
         List<Object> response = new ArrayList();
