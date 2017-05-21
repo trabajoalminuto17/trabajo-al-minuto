7950d3729896176da76ebe780265d8378d190f41
Johnny <Johnny@Johnny-PC>
2017-05-21 14:53:15 -0500

    Merge origin/master
    Conflicts:
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/java/com/basp/trabajo_al_minuto/web/model/AtributosWeb.java
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/java/com/basp/trabajo_al_minuto/web/model/ComponenteWeb.java
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/java/com/basp/trabajo_al_minuto/web/view/CrearUsuarioView.java
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/java/com/basp/trabajo_al_minuto/web/view/PortalView.java
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/webapp/modulos/usuarios/crear.xhtml
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/webapp/portal.xhtml
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/webapp/resources/template/main_portal.xhtml
    	trabajo_al_minuto/trabajo_al_minuto-web/src/main/webapp/resources/template/topbar_portal.xhtml

--- trabajo_al_minuto/trabajo_al_minuto-model/src/main/java/com/basp/trabajo_al_minuto/model/business/BusinessHtmlTemplates.java
+++ trabajo_al_minuto/trabajo_al_minuto-model/src/main/java/com/basp/trabajo_al_minuto/model/business/BusinessHtmlTemplates.java
@@ -34,31 +34,30 @@ public class BusinessHtmlTemplates {
         return response.toString();
     }
 
-    public static String enviarCitacion(String empresa, Date fecha, String name, String lugar, String detalles, String tituloOferta, String telefono) throws BusinessException {
-        StringBuilder response = new StringBuilder();
-        response.append("<center><table width='800' border='0' cellspacing='0' cellpadding='5'>");
-        response.append("<tbody><tr><td align='center'><table width='800' border='0' cellspacing='0' cellpadding='0'>");
-        response.append("<tbody><tr><td width='800' style='border-bottom: solid 1px #ccc;'><img src='http://158.69.206.240:8080/trabajo_al_minuto-web/resources/image/logo_h.png' width='300' height='100' alt='Trabajo al minuto' style='float:right;'/></td></tr>");
-        response.append("<tr><td bgcolor='#FFFFFF'><table width='800' border='0' cellpadding='10' cellspacing='0'>");
-        response.append("<tbody><tr><td style='border-bottom: solid 1px #ccc;' width='923'>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Hola, ").append(name).append(". </p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Te informamos que has sido seleccionado por nuestra área de talento humano para continuar en el proceso de selección de: <strong>").append(tituloOferta).append("</strong>.</p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Los datos de tu citación son:</p> ");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Empresa: <strong>").append(empresa).append("</strong></p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Fecha: <strong>").append(fecha).append("</strong></p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Lugar: <strong>").append(lugar).append("</strong></p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Detalles: <strong>").append(detalles).append("</strong></p><br>");
-        response.append("</td></tr></tbody></table></td></tr>");
-        response.append("<tr><td style='border-bottom: solid 1px #ccc;' width='759' >");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'><strong>¿Preguntas?</strong> Puede comunicarse con nosotros al ").append(telefono).append("</p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'>o al correo: citaciones@trabajo_al_minuto.com.</p>");
-        response.append("<br><br>");
-        response.append("</td></tr>");
-        response.append("</tbody></table></center>");
-
-        return response.toString();
-    }
-
+//    public static String enviarCitacion(String empresa, Date fecha, String name, String lugar, String detalles, String tituloOferta, String telefono) throws BusinessException {
+//        StringBuilder response = new StringBuilder();
+//        response.append("<center><table width='800' border='0' cellspacing='0' cellpadding='5'>");
+//        response.append("<tbody><tr><td align='center'><table width='800' border='0' cellspacing='0' cellpadding='0'>");
+//        response.append("<tbody><tr><td width='800' style='border-bottom: solid 1px #ccc;'><img src='http://158.69.206.240:8080/trabajo_al_minuto-web/resources/image/logo_h.png' width='300' height='100' alt='Trabajo al minuto' style='float:right;'/></td></tr>");
+//        response.append("<tr><td bgcolor='#FFFFFF'><table width='800' border='0' cellpadding='10' cellspacing='0'>");
+//        response.append("<tbody><tr><td style='border-bottom: solid 1px #ccc;' width='923'>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Hola, ").append(name).append(". </p>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Te informamos que has sido seleccionado por nuestra área de talento humano para continuar en el proceso de selección de: <strong>").append(tituloOferta).append("</strong>.</p>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Los datos de tu citación son:</p> ");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Empresa: <strong>").append(empresa).append("</strong></p>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Fecha: <strong>").append(fecha).append("</strong></p>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Lugar: <strong>").append(lugar).append("</strong></p>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Detalles: <strong>").append(detalles).append("</strong></p><br>");
+//        response.append("</td></tr></tbody></table></td></tr>");
+//        response.append("<tr><td style='border-bottom: solid 1px #ccc;' width='759' >");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'><strong>¿Preguntas?</strong> Puede comunicarse con nosotros al ").append(telefono).append("</p>");
+//        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'>o al correo: citaciones@trabajo_al_minuto.com.</p>");
+//        response.append("<br><br>");
+//        response.append("</td></tr>");
+//        response.append("</tbody></table></center>");
+//
+//        return response.toString();
+//    }
     public static String enviarCitacion(String empresa, String fecha, String name, String lugar, String detalles, String tituloOferta) throws BusinessException {
         StringBuilder response = new StringBuilder();
         response.append("<center><table width='800' border='0' cellspacing='0' cellpadding='5'>");
@@ -92,7 +91,7 @@ public class BusinessHtmlTemplates {
         response.append("<tr><td bgcolor='#FFFFFF'><table width='800' border='0' cellpadding='10' cellspacing='0'>");
         response.append("<tbody><tr><td style='border-bottom: solid 1px #ccc;' width='923'>");
         response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Hola, ").append(name).append(". </p>");
-        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Queremos informarte que la citación ha sido reprogramada, pedimos disculpas por los inconvenientes que esto pueda ocasionar.");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Queremos informarte que la citación ha sido reprogramada.");
         response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Los nuevos datos de tu citación son:</p> ");
         response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Empresa: <strong>").append(empresa).append("</strong></p>");
         response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Fecha: <strong>").append(fecha).append("</strong></p>");
@@ -101,6 +100,29 @@ public class BusinessHtmlTemplates {
         response.append("</td></tr></tbody></table></td></tr>");
         response.append("<tr><td style='border-bottom: solid 1px #ccc;' width='759' >");
         response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'><strong>¿Preguntas?</strong> Puede comunicarse con nosotros al 7456345 o al correo: citaciones@trabajo_al_minuto.com </p><br>");
+        response.append("<br><br>");
+        response.append("</td></tr>");
+        response.append("</tbody></table></center>");
+
+        return response.toString();
+    }
+
+    public static String citacionRechazada(String titulo, String fecha, String name, String lugar, String detalles) throws BusinessException {
+        StringBuilder response = new StringBuilder();
+        response.append("<center><table width='800' border='0' cellspacing='0' cellpadding='5'>");
+        response.append("<tbody><tr><td align='center'><table width='800' border='0' cellspacing='0' cellpadding='0'>");
+        response.append("<tbody><tr><td width='800' style='border-bottom: solid 1px #ccc;'><img src='http://158.69.206.240:8080/trabajo_al_minuto-web/resources/image/logo_h.png' width='300' height='100' alt='Trabajo al minuto' style='float:right;'/></td></tr>");
+        response.append("<tr><td bgcolor='#FFFFFF'><table width='800' border='0' cellpadding='10' cellspacing='0'>");
+        response.append("<tbody><tr><td style='border-bottom: solid 1px #ccc;' width='923'>");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Hola, ").append(name).append(". </p>");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> La citación programada para el participante ").append(name).append(" ha sido rechazada. </p>");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Los datos de la citación son:</p> ");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Titulo oferta: <strong>").append(titulo).append("</strong></p>");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Fecha: <strong>").append(fecha).append("</strong></p>");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Lugar: <strong>").append(lugar).append("</strong></p>");
+        response.append("<p style=' color:#4D4D4D; font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif'> Detalles: <strong>").append(detalles).append("</strong></p><br>");
+        response.append("</td></tr></tbody></table></td></tr>");
+        response.append("<tr><td style='border-bottom: solid 1px #ccc;' width='759' >");
         response.append("<br><br>");
         response.append("</td></tr>");
         response.append("</tbody></table></center>");
